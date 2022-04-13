fe57e2d0ae032d8b96ba9d2f66aaff95d1df0407
saikrishnakathroji <89527892+saikrishnakathroji@users.noreply.github.com>
2022-04-13 19:49:38 +0530

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    
+    
+        public struct Book
+        {
+            int BookId;
+            string Title;
+            int Price;
+            string bookType;
+            enum BookType { Magazine, Novel, ReferenceBook, Miscellaneous };
+            public void Display()
+            {
+                Console.WriteLine("The Details Of The Book:BookId={0},Title={1},Price={2},bookType={3}", BookId, Title, Price, bookType);
+            }
+
+            public static void Main(string[] args)
+            {
+                Book b1 = new Book();
+                b1.BookId = 1;
+                b1.Title = "Think Like A Monk";
+                b1.Price = 350;
+                b1.bookType = Convert.ToString(BookType.Magazine);
+                b1.Display();
+
+            }
+        }
+
+    
+}
